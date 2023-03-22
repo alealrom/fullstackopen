8db235065b5a28c811cb27be166eb75242d4bbac
alealrom <alealrom@yahoo.com>
2023-03-22 15:14:15 -0500

    update Excercise 1.11 of Part1 has been resolved.

--- Part 1/unicafe/src/index.js
+++ Part 1/unicafe/src/index.js
@@ -1,75 +1,77 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
 const Button = (props) => {
-  return (
-    <button onClick={props.handleClick}>{props.text}</button>
-   );
+  return <button onClick={props.handleClick}>{props.text}</button>;
 };
 
 const Statistics = ({ good, neutral, bad }) => {
   const allFeedbacks = good + neutral + bad;
-  const average = ((good - bad) / allFeedbacks);
-  const positive = (good * 100 / allFeedbacks);
+  const average = (good - bad) / allFeedbacks;
+  const positive = (good * 100) / allFeedbacks;
 
-  if (allFeedbacks === 0 || isNaN(average) || isNaN(positive)){
-    return(
+  if (allFeedbacks === 0 || isNaN(average) || isNaN(positive)) {
+    return (
       <main>
         <h2>Statistics</h2>
         <p>No feedback given.</p>
       </main>
-    )
+    );
   }
 
   return (
     <main>
       <h2>Statistics</h2>
-      <Statistic text='Good' value={good}/>
-      <Statistic text='Neutral' value={neutral}/>
-      <Statistic text='Bad' value={bad}/>
-      <Statistic text='All' value={allFeedbacks}/>
-      <Statistic text='Average' value={average}/>
-      <Statistic text='Positive' value={positive}/>
+      <Statistic text="Good" value={good} />
+      <Statistic text="Neutral" value={neutral} />
+      <Statistic text="Bad" value={bad} />
+      <Statistic text="All" value={allFeedbacks} />
+      <Statistic text="Average" value={average} />
+      <Statistic text="Positive" value={positive + "%"} />
     </main>
   );
 };
 
 const Statistic = ({ text, value }) => {
   return (
+    <table>
+      <tbody>
+        <tr>
+          <td>{text}</td>
+          <td className="value">{value}</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGoodClick = () => {
+    setGood(good + 1);
+  };
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const handleBadClick = () => {
+    setBad(bad + 1);
+  };
+
+  return (
     <main>
-      <p>{text} {value}</p>
+      <h2>Give Feedback</h2>
+      <Button handleClick={handleGoodClick} text="Good" />
+      <Button handleClick={handleNeutralClick} text="Neutral" />
+      <Button handleClick={handleBadClick} text="Bad" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </main>
-  )
-}
+  );
+};
 
-const App = () =>{
-const [ good, setGood ] = useState(0);
-const [ neutral, setNeutral ] = useState(0);
-const [ bad, setBad ] = useState(0);
-
-
-const handleGoodClick = () => {
-  setGood(good + 1);
-}
-
-const handleNeutralClick = () => {
-  setNeutral(neutral + 1);
-}
-
-const handleBadClick = () => {
-  setBad(bad + 1);
-}
-
-return (
-  <main>
-    <h2>Give Feedback</h2>
-    <Button handleClick={handleGoodClick} text='Good'/>
-    <Button handleClick={handleNeutralClick} text='Neutral'/>
-    <Button handleClick={handleBadClick} text='Bad'/>
-    <Statistics good={good} neutral={neutral} bad={bad}/>
-  </main>
-)
-}
-
-ReactDOM.render(<App/>, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
